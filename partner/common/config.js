2228133176f67f25604694229beeeba7828911df
Xin0616 <1014692922@qq.com>
2019-08-22 14:09:03 +0800

    1

--- partner/common/config.js
+++ partner/common/config.js
@@ -4,6 +4,6 @@ config.host = process.env.NODE_ENV === 'development'
 	? 
 		"http://192.168.191.1:8082"
 	: 
-		'https://motiactivity.gray.motimall.cn/api'
+		'https://'+webHost+'/api'
 	//	"https://daojia.motivape.cn/api" // 生产环境/线上环境
 export default config
